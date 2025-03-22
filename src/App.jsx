7c30f7d53df96c4a10088ebeb6be17e3187ba831
Manisha Rana <manisharana02005@gmail.com>
2025-03-22 11:12:58 +0530

    Completed the structure and css of Random gif component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 
+import Random from "./components/Random"
+import Tag from "./components/Tag"
+
+
 export default function App(){
   
-  return <div>Random GIF</div>;
+  return (
+    <div className=" h-screen flex flex-col background relative overflow-hidden items-center">
+      <h1 className=" bg-white rounded-sm text-center mt-[40px]  text-4xl font-bold w-11/12 px-10 py-2">RANDOM GIFS</h1>
+
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+        <Random/>
+        <Tag/>
+      </div>
+    </div>
+  )
 }
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react"
+
 export default function Random() {
-  return <div>Random</div>;
+
+  const [gif,setGif]=useState('');
+
+  function clickHandler(){
+
+  }
+
+
+  return(
+    <div className="w-1/2 h-[450px] bg-green-400 rounded-lg border border-black flex flex-col gap-y-5 mt-[15px] items-center">
+      <h1 className="text-2xl underline uppercase font-bold">A Random Gif</h1>
+      <img src={gif} width="450"/>
+      <button className="w-10/12 bg-green-100 py-2 px-10 rounded-xl text-2xl font-bold" onClick={clickHandler}>Genrerate</button>
+    </div>
+  )
 }
