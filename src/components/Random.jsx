3ea692f8d3e524a1f33ad2f4de6a81c7dbd5d292
Manisha Rana <manisharana02005@gmail.com>
2025-03-22 12:16:13 +0530

    Added API to fetch random gifs

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,19 +1,41 @@
-import { useState } from "react"
+
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+const API_KEY=process.env.REACT_APP_GIPHY_API_KEY;
 
 export default function Random() {
 
-  const [gif,setGif]=useState('');
+  const [gif , setGif]=useState('');
+  const [loading, setLoading]= useState(false);
+  
+
+  async function fetchData(params) {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const {data} = await axios.get(url);
+    // console.log(output);
+    const imageSource=data.data.images.downsized_large.url;
+    setGif(imageSource);
+    
+  }
+
+  useEffect(()=>{
+    fetchData();
+  }, [])
 
   function clickHandler(){
-
+    fetchData();
   }
 
 
   return(
-    <div className="w-1/2 h-[450px] bg-green-400 rounded-lg border border-black flex flex-col gap-y-5 mt-[15px] items-center">
+    <div className="w-1/2  bg-green-400 rounded-lg border border-black flex flex-col gap-y-5 mt-[15px] items-center">
       <h1 className="text-2xl underline uppercase font-bold">A Random Gif</h1>
+
+     
       <img src={gif} width="450"/>
-      <button className="w-10/12 bg-green-100 py-2 px-10 rounded-xl text-2xl font-bold" onClick={clickHandler}>Genrerate</button>
+
+      <button className="w-10/12 bg-green-100 py-2 px-10 rounded-xl text-2xl font-bold mb-[20px]" onClick={clickHandler}>Genrerate</button>
     </div>
   )
 }
